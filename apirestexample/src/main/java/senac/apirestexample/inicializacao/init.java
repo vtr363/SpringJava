db58fb09829dcef3e1827547855910b8620d97d5
ADS 2022.1N <36124872022.1N@senacrjedu.local>
2023-08-17 20:58:51 -0300

    first

--- apirestexample/src/main/java/senac/apirestexample/inicializacao/init.java
+++ apirestexample/src/main/java/senac/apirestexample/inicializacao/init.java
@@ -1,13 +1,29 @@
 package senac.apirestexample.inicializacao;
 
+import org.hibernate.bytecode.internal.bytebuddy.PrivateAccessorException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 
+import senac.apirestexample.entity.Aluno;
+import senac.apirestexample.service.AlunoService;
+
 public class init implements ApplicationListener<ContextRefreshedEvent>{
 
+	
+	@Autowired
+	private AlunoService alunoService;
+	
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent event) {
 		// TODO Auto-generated method stub
+		
+		Aluno aluno1 = new Aluno();
+		aluno1.setEmail("lucas@gmail.com");
+		aluno1.setNome("Lucas");
+		aluno1.setSobreNome("Silva");
+		alunoService.salvar(aluno1);
+		
 		
 	}
 
--- apirestexample/src/main/java/senac/apirestexample/service/AlunoService.java
+++ apirestexample/src/main/java/senac/apirestexample/service/AlunoService.java
@@ -1,5 +1,25 @@
 package senac.apirestexample.service;
 
-public class AlunoService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import senac.apirestexample.entity.Aluno;
+import senac.apirestexample.repository.AlunoRepository;
+
+@Service
+public class AlunoService {
+	
+	@Autowired
+	AlunoRepository repo;
+	public Aluno salvar(Aluno aluno) {
+		return repo.save(aluno);
+	}
+	
+			
+	@Autowired
+	public List<Aluno> buscaTodosAlunos() {
+		return repo.findAll();
+	}
 }
